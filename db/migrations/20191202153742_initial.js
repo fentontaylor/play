cc36bd16f5c88df6144b4b231585221857a5a19a
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-02 16:09:22 -0700

    Add default unknown to genre in favorites table
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>

--- db/migrations/20191202153742_initial.js
+++ db/migrations/20191202153742_initial.js
@@ -3,10 +3,11 @@ exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('favorites', function(table) {
       table.increments('id').primary();
-      table.string('title');
-      table.string('artist_name');
-      table.string('genre');
-      table.integer('rating');
+      table.string('title').notNullable();
+      table.string('artist_name').notNullable();
+      table.string('genre').defaultTo('Unknown').notNullable();
+      table.integer('rating').notNullable();
+
 
       table.timestamps(true, true);
     })

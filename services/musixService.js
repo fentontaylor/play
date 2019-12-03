67b78dfafc4c48694750769b06118c28d7b3c17e
fentontaylor <fentontaylor@gmail.com>
2019-12-03 14:14:14 -0700

    Add fetchSongInfo to MusixService, passes test

--- services/musixService.js
+++ services/musixService.js
@@ -2,12 +2,21 @@ const fetch = require('node-fetch');
 
 class MusixService {
   constructor(title, artist) {
-    this.songTitle = title,
-    this.artistName = artist
+    this.key = process.env.MUSIXMATCH_KEY,
+    this.url = `https://api.musixmatch.com/ws/1.1/matcher.track.get` +
+      `?q_track=${title}` +
+      `&q_artist=${artist}` +
+      `&apikey=${this.key}`
   }
 
   async fetchSongInfo() {
-
+    try {
+      let res = await fetch(this.url);
+      let json = await res.json();
+      return json;
+    } catch(e) {
+      return e;
+    }
   }
 }
 
--- tests/services/musixService.spec.js
+++ tests/services/musixService.spec.js
@@ -1,8 +1,21 @@
 var app = require('../../app');
+const MusixService = require('../../services/musixService');
 
 describe("MusixService", ()=> {
   it("can initialize", ()=> {
     let service = new MusixService("We Will Rock You", "Queen");
+
     expect(service).toBeInstanceOf(MusixService);
+  })
+
+  it("can fetch song details from musixmatch API", async ()=> {
+    let service = await new MusixService("We Will Rock You", "Queen");
+    let res = await service.fetchSongInfo();
+    
+    let track = res.message.body.track;
+    
+    expect(res).toHaveProperty("message");
+    expect(track).toHaveProperty("track_rating");
+    expect(track).toHaveProperty("primary_genres");
   })
 })

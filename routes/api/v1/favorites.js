ef6c859934a55b412c29ca7dc29b7a68ef1caba4
fentontaylor <fentontaylor@gmail.com>
2019-12-03 13:55:20 -0700

    Remove lines that check the rating values

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,7 +6,6 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const Favorite = require("../../../models/favorite");
 
-
 router.get('/', (request, response) => {
   favoriteSongs()
     .then(favorites => {
@@ -16,6 +15,8 @@ router.get('/', (request, response) => {
 
 router.post('/', (request, response) => {
   var body = request.body;
+  const title = body.title;
+  const artist = body.artistName;
 
   for (let requiredParam of ['title', 'artistName']) {
     if (!body[requiredParam]) {
@@ -25,21 +26,25 @@ router.post('/', (request, response) => {
     }
   }
 
-  var rating = parseInt(request.body.rating)
+  // var rating = parseInt(request.body.rating)
 
-  if (!(rating >= 1 && rating <= 100)) {
-    return response
-      .status(400)
-      .send({ error: "Rating must be between 1-100" })
-  }
+  // if (!(rating >= 1 && rating <= 100)) {
+  //   return response
+  //     .status(400)
+  //     .send({ error: "Rating must be between 1-100" })
+  // }
 
-  var fav = new Favorite(body);
+  fetchSongInfo(title, artist)
+  .then(res => {
+    console.log(res);
+    var fav = new Favorite(res);
 
-  database('favorites')
-    .insert(fav, 'id')
-    .returning('id')
-    .then(id => response.status(201).send({ id: id[0] }))
-    .catch(error => response.status(500).send({ error }))
+    database('favorites')
+      .insert(fav, 'id')
+      .returning('id')
+      .then(id => response.status(201).send({ id: id[0] }))
+      .catch(error => response.status(500).send({ error }))
+  })
 });
 
 async function favoriteSongs() {
@@ -49,7 +54,5 @@ async function favoriteSongs() {
     return e;
   }
 }
-
-
 
 module.exports = router;

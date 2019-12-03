3a7c7852251ba507339ac6e02a5e3ecc5342f628
fentontaylor <fentontaylor@gmail.com>
2019-12-02 20:04:53 -0700

    Add sad path test for missing required attributes

--- tests/favorites/create.spec.js
+++ tests/favorites/create.spec.js
@@ -22,7 +22,7 @@ describe("Test POST to favorites", () => {
       "artistName": "Queen",
       "genre": "Rock",
       "rating": 88
-    }
+    };
 
     const noFavs = await database('favorites').first()
     expect(noFavs).toBeUndefined();
@@ -38,5 +38,67 @@ describe("Test POST to favorites", () => {
     expect(fav.artist_name).toBe(body.artistName);
     expect(fav.genre).toBe(body.genre);
     expect(fav.rating).toBe(body.rating);
+  })
+
+  it("happy path with default genre", async ()=> {
+    const body = {
+      "id": 1,
+      "title": "We Will Rock You",
+      "artistName": "Queen",
+      "rating": 88
+    };
+
+    const res = await request(app)
+      .post("/api/v1/favorites")
+      .send(body);
+
+    const fav = await database('favorites').first();
+
+    expect(res.statusCode).toBe(201);
+    expect(fav.genre).toBe("Unknown");
+  })
+
+  it("sad path: missing required attribute", async ()=> {
+    var body = {
+      "id": 1,
+      "artistName": "Queen",
+      "genre": "Rock",
+      "rating": 88
+    };
+
+    var res = await request(app)
+      .post("/api/v1/favorites")
+      .send(body);
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toMatchObject({ error: 'Missing required attribute <title>' })
+
+    var body = {
+      "id": 1,
+      "title": "We Will Rock You",
+      "genre": "Rock",
+      "rating": 88
+    };
+
+    var res = await request(app)
+      .post("/api/v1/favorites")
+      .send(body);
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toMatchObject({ error: 'Missing required attribute <artistName>' })
+
+    var body = {
+      "id": 1,
+      "title": "We Will Rock You",
+      "artistName": "Queen",
+      "genre": "Rock",
+    };
+
+    var res = await request(app)
+      .post("/api/v1/favorites")
+      .send(body);
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toMatchObject({ error: 'Missing required attribute <rating>' })
   })
 })

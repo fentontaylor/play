573611c61eb97547543f798e3dca483f189989d8
fentontaylor <fentontaylor@gmail.com>
2019-12-04 12:05:54 -0700

    Add catch for 500 errors to all .thens in favorites routes
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -11,28 +11,30 @@ const createFavorite = favoritesHelpers.createFavorite;
 
 router.get('/', (request, response) => {
   favoriteSongs()
-    .then(favorites => {
-      if (favorites.length) {
-        response.status(200).send(favorites)
-      } else {
-        response.status(404).json({
-          error: 'Not found.'
-        })
-      }
-    })
+  .then(favorites => {
+    if (favorites.length) {
+      response.status(200).send(favorites)
+    } else {
+      response.status(404).json({
+        error: 'Not found.'
+      })
+    }
+  })
+  .catch(error => response.status(500).send({ error }))
 });
 
 router.get('/:id', (request, response) => {
   favoriteSong(request.params.id)
-    .then(favorite => {
-      if (favorite.length) {
-        response.status(200).send(favorite)
-      } else {
-        response.status(404).json({
-          error: 'Record not found.'
-        })
-      }
-    })
+  .then(favorite => {
+    if (favorite.length) {
+      response.status(200).send(favorite)
+    } else {
+      response.status(404).json({
+        error: 'Record not found.'
+      })
+    }
+  })
+  .catch(error => response.status(500).send({ error }))
 });
 
 router.post('/', (request, response) => {
@@ -60,21 +62,24 @@ router.post('/', (request, response) => {
     })
     .catch(error => response.status(500).send({ error }))
   })
+  .catch(error => response.status(500).send({ error }))
 });
 
 router.delete('/:id', (request, response) => {
   favoriteSong(request.params.id)
-    .then(favorite => {
-      if (favorite.length) {
-        let targetId = request.params.id
-        seekAndDestroy(targetId)
-        .then(() => response.status(204).send())
-      } else {
-        response.status(404).json({
-          error: 'Record not found.'
-        })
-      }
-    })
+  .then(favorite => {
+    if (favorite.length) {
+      let targetId = request.params.id
+      seekAndDestroy(targetId)
+      .then(() => response.status(204).send())
+      .catch(error => response.status(500).send({ error }))
+    } else {
+      response.status(404).json({
+        error: 'Record not found.'
+      })
+    }
+  })
+  .catch(error => response.status(500).send({ error }))
 });
 
 module.exports = router;

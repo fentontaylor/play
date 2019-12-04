2e49ea1ab96e28b41eda5aaefddcbb2340aceef7
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-03 21:34:13 -0700

    Add test for destroying favorites by id

---
+++ tests/favorites/destroy.spec.js
@@ -0,0 +1,48 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../../knexfile')[environment];
+const database = require('knex')(configuration);
+const Favorite = require("../../models/favorite");
+
+describe("Test DELETE favorites", () => {
+  beforeEach(async () => {
+    await database.raw("TRUNCATE TABLE favorites CASCADE");
+  });
+
+  afterEach(async () => {
+    await database.raw("TRUNCATE TABLE favorites CASCADE");
+  });
+
+  it("deletes a single favorite by id passed in body as json", async () => {
+
+    await database('favorites').insert({id: 1, title: 'Thunderstruck', artist_name: 'AC/DC', genre: 'Rock', rating: 98})
+
+    var body = {
+      "id": 1
+    };
+
+    var response = await request(app)
+    .delete('/api/v1/favorites')
+    .send(body);
+
+    expect(response.status).toBe(204)
+  });
+
+  it("returns a 404 if id is not found", async () => {
+
+    var response = await request(app)
+    var body = {
+      "id": 1
+    };
+
+    var response = await request(app)
+    .delete('/api/v1/favorites')
+    .send(body);
+
+    expect(response.status).toBe(404)
+    expect(response.body.error).toBe('Record not found.')
+  });
+});

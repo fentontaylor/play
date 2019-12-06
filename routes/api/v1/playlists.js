0b1e1a37b49f1dfed5d9988f3cc4100ad85bb23b
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-05 19:21:02 -0700

    Add post endpoint to create playlists

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
 
+router.post('/', (request, response) => {
+  var body = request.body;
+  var title = body.title;
+
+  for (let requiredParam of ['title']) {
+    if (!body[requiredParam]){
+      return response
+        .status(400)
+        .send({ error: `Missing required attribute <${requiredParam}>` });
+    }
+  }
+  createPlaylist(title)
+  .then(data => {
+    response.status(201).send(data[0])
+  })
+  .catch(error => response.status(500).send({error}))
+});
+
+
+async function createPlaylist(title) {
+  try {
+    return await database('playlists')
+      .insert({title: title}, 'id')
+      .returning('*')
+  } catch(e) {
+    return e;
+  }
+}
 
 module.exports = router;

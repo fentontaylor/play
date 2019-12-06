608574278b79f6cd06028af72611baccd2aebe61
fentontaylor <fentontaylor@gmail.com>
2019-12-05 18:48:25 -0700

    Add happy path logic for deleting playlist

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const helpers = require('../../../utils/playlistsHelpers');
+const findPlaylist = helpers.findPlaylist;
+const deletePlaylist = helpers.deletePlaylist;
 
-
+router.delete('/:id', (request, response) => {
+  deletePlaylist(request.params.id)
+  .then(() => response.status(204).send())
+  .catch(error => response.status(500).send({ error }));
+})
 
 module.exports = router;

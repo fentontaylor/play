60551e2836970cb5d47003f2cff9120954facb7c
fentontaylor <fentontaylor@gmail.com>
2019-12-06 12:19:53 -0700

    Add sad path tests for  missing title attribute

--- tests/playlists/update.spec.js
+++ tests/playlists/update.spec.js
@@ -39,4 +39,26 @@ describe('PUT /api/v1/playlists/:id', () => {
     expect(res.status).toBe(404);
     expect(res.body).toEqual({ error: 'Record not found' });
   })
+
+  it('returns 400 if title not provided', async () => {
+    let res1 = await request(app)
+      .put('/api/v1/playlists/1')
+
+    expect(res1.status).toBe(400);
+    expect(res1.body).toEqual({ error: 'Missing required attribute <title>' });
+
+    let res2 = await request(app)
+      .put('/api/v1/playlists/1')
+      .send({ title: '' });
+
+    expect(res2.status).toBe(400);
+    expect(res2.body).toEqual({ error: 'Missing required attribute <title>' });
+
+    let res3 = await request(app)
+      .put('/api/v1/playlists/1')
+      .send({ title: 0 });
+
+    expect(res3.status).toBe(400);
+    expect(res3.body).toEqual({ error: 'Missing required attribute <title>' });
+  })
 })

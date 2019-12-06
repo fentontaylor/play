a35f724c676a744d376d77715dd0c943f81550af
fentontaylor <fentontaylor@gmail.com>
2019-12-05 19:03:48 -0700

    Add logic to find playlist first before deleting, return 404 if not found

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -5,8 +5,18 @@ const findPlaylist = helpers.findPlaylist;
 const deletePlaylist = helpers.deletePlaylist;
 
 router.delete('/:id', (request, response) => {
-  deletePlaylist(request.params.id)
-  .then(() => response.status(204).send())
+  let id = request.params.id;
+
+  findPlaylist(id)
+  .then(result => {
+    if (result) {
+      deletePlaylist(request.params.id)
+      .then(() => response.status(204).send())
+      .catch(error => response.status(500).send({ error }));
+    } else {
+      response.status(404).send({ error: 'Record not found' })
+    }
+  })
   .catch(error => response.status(500).send({ error }));
 })
 

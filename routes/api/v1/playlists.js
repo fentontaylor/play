8a52bf2a191f60710c19f86089187a6fe3ffedea
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-12-05 20:06:35 -0700

    Refactored based on feedback from team member and added a test for 400 error

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -9,20 +9,16 @@ router.post('/', (request, response) => {
   var body = request.body;
   var title = body.title;
 
-  for (let requiredParam of ['title']) {
-    if (!body[requiredParam]){
-      return response
-        .status(400)
-        .send({ error: `Missing required attribute <${requiredParam}>` });
-    }
+  if (!title){
+    return response.status(400).send({ error: `Missing required attribute <title>` });
   }
+
   createPlaylist(title)
   .then(data => {
     response.status(201).send(data[0])
   })
   .catch(error => response.status(500).send({error}))
 });
-
 
 async function createPlaylist(title) {
   try {

817c76c031a2a0f5dbcabfbc6ceec480c18bb76b
fentontaylor <fentontaylor@gmail.com>
2019-12-08 15:23:25 -0700

    Add migration for playlist_favorites

--- db/migrations/20191208142915_createPlaylistFavorites.js
+++ db/migrations/20191208142915_createPlaylistFavorites.js
@@ -1,16 +1,12 @@
 
-exports.up = function(knex) {
-  return Promise.all([
-    knex.schema.createTable('playlist_favorites', function(t) {
-      t.increments('id').primary();
-      t.integer('playlist_id').unsigned().references('id').inTable('playlists');
-      t.integer('favorite_id').unsigned().references('id').inTable('favorites');
-    })
-  ])
+exports.up = (knex) => {
+  return knex.schema.createTable('playlist_favorites', (t) => {
+    t.increments('id').primary();
+    t.integer('playlist_id').unsigned().notNullable().references('playlists.id');
+    t.integer('favorite_id').unsigned().notNullable().references('favorites.id');
+  })
 };
 
-exports.down = function(knex) {
-  return Promise.all([
-    knex.schema.dropTable('playlist_favorites')
-  ])
+exports.down = (knex) => {
+  return knex.schema.dropTable('playlist_favorites')
 };

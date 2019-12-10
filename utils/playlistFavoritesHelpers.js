948ea8838df874dfc222a2d15191bbd7ef8e9d01
fentontaylor <fentontaylor@gmail.com>
2019-12-09 19:03:33 -0700

    Add function playlistInfo() to playlistFavoritesHelpers util

--- utils/playlistFavoritesHelpers.js
+++ utils/playlistFavoritesHelpers.js
@@ -1,8 +1,10 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
+const plHelpers = require('./playlistsHelpers');
+const findPlaylist = plHelpers.findPlaylist;
 
-const createPlaylistFavorite = async function(playlistId, favoriteId) {
+async function createPlaylistFavorite(playlistId, favoriteId) {
   try {
     let result = await database('playlist_favorites')
       .insert({ playlist_id: playlistId, favorite_id: favoriteId }, 'id')
@@ -13,7 +15,7 @@ const createPlaylistFavorite = async function(playlistId, favoriteId) {
   }
 }
 
-const allPlaylistFavorites = async function(playlistId) {
+async function allPlaylistFavorites(playlistId) {
   try {
     let res = await database('playlist_favorites')
       .select('favorites.id', 'favorites.title', 'favorites.artist_name', 'favorites.genre', 'favorites.rating')
@@ -26,7 +28,7 @@ const allPlaylistFavorites = async function(playlistId) {
   }
 }
 
-const countFavorites = async function(playlistId) {
+async function countFavorites(playlistId) {
   try {
     let res =  await database('playlist_favorites')
       .join('favorites', { 'favorites.id': 'playlist_favorites.favorite_id' })
@@ -37,7 +39,7 @@ const countFavorites = async function(playlistId) {
   }
 }
 
-const songAvgRating = async function(playlistId) {
+async function songAvgRating(playlistId) {
   try {
     let res = await database('playlist_favorites')
       .join('favorites', { 'favorites.id': 'playlist_favorites.favorite_id' })
@@ -49,9 +51,31 @@ const songAvgRating = async function(playlistId) {
   }
 }
 
+async function playlistInfo(playlistId) {
+  try {
+    let info = await findPlaylist(playlistId);
+    let songCount = await countFavorites(playlistId);
+    let avgRating = await songAvgRating(playlistId);
+    let favorites = await allPlaylistFavorites(playlistId);
+
+    return {
+      id: info.id,
+      title: info.title,
+      songCount: songCount,
+      songAvgRating: avgRating,
+      favorites: favorites,
+      createdAt: info.created_at,
+      updatedAt: info.updated_at
+    }
+  } catch(e) {
+    return e;
+  }
+}
+
 module.exports = {
   createPlaylistFavorite: createPlaylistFavorite,
   allPlaylistFavorites: allPlaylistFavorites,
   countFavorites: countFavorites,
-  songAvgRating: songAvgRating
+  songAvgRating: songAvgRating,
+  playlistInfo: playlistInfo
 }

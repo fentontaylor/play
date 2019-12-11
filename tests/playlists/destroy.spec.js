95c5cad56df6235670a06d203c425751f30db797
fentontaylor <fentontaylor@gmail.com>
2019-12-10 21:04:02 -0700

    Add test for cascading delete when deleting playlist

--- tests/playlists/destroy.spec.js
+++ tests/playlists/destroy.spec.js
@@ -8,11 +8,15 @@ const database = require('knex')(configuration);
 
 describe("DELETE /api/v1/playlists/:id", () => {
   beforeEach(async () => {
+    await database.raw("TRUNCATE TABLE playlist_favorites CASCADE");
     await database.raw("TRUNCATE TABLE playlists CASCADE");
+    await database.raw("TRUNCATE TABLE favorites CASCADE");
   });
 
   afterEach(async () => {
+    await database.raw("TRUNCATE TABLE playlist_favorites CASCADE");
     await database.raw("TRUNCATE TABLE playlists CASCADE");
+    await database.raw("TRUNCATE TABLE favorites CASCADE");
   });
 
   it("deletes a single playlist by id", async () => {
@@ -38,5 +42,54 @@ describe("DELETE /api/v1/playlists/:id", () => {
 
     expect(res.status).toBe(404);
     expect(res.body).toEqual({ error: 'Record not found' });
+  })
+
+  it('deletes related playlist_favorites when deleted', async () => {
+    await database('playlists').insert([
+      { id: 1, title: 'Jams' },
+      { id: 2, title: 'Beats' }
+    ]);
+
+    await database('favorites').insert(
+      { 
+        id: 1,
+        title: 'Under Pressure',
+        artist_name: 'Queen',
+        genre: 'Rock',
+        rating: 75
+      }
+    );
+
+    await database('playlist_favorites').insert([
+      { id: 1, playlist_id: 1, favorite_id: 1 },
+      { id: 2, playlist_id: 2, favorite_id: 1 }
+    ]);
+
+    // Before delete: 2 playlists, 2 playlist_favorites, 1 favorite
+    var playlists = await database('playlists').select()
+
+    expect(playlists.length).toBe(2);
+
+    var playlistFavorites = await database('playlist_favorites').select()
+
+    expect(playlistFavorites).toEqual([
+      { id: 1, playlist_id: 1, favorite_id: 1 },
+      { id: 2, playlist_id: 2, favorite_id: 1 }
+    ])
+
+    // Delete playlist with id 1
+    await request(app)
+      .delete('/api/v1/playlists/1');
+
+    // After delete: 1 playlist, 1 playlist favorite, 1 favorite
+    var playlists = await database('playlists').select()
+
+    expect(playlists.length).toBe(1);
+
+    var playlistFavorites = await database('playlist_favorites').select()
+
+    expect(playlistFavorites).toEqual([
+      { id: 2, playlist_id: 2, favorite_id: 1 }
+    ])
   })
 })

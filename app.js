a345e7bb70ec72e717e511c2de930d4a638439c6
fentontaylor <fentontaylor@gmail.com>
2020-02-02 15:29:09 -0700

    Remove unnecessary lines in app.js and knexfile.js

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-
-var indexRouter = require('./routes/index');
-var favoritesRouter = require('./routes/api/v1/favorites');
-var playlistsRouter = require('./routes/api/v1/playlists');
-var playlistFavoritesRouter = require('./routes/api/v1/playlistFavorites');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -19,11 +11,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//////////////
+//  API V1  //
+//////////////
+var indexRouter = require('./routes/index');
+var favoritesRouter = require('./routes/api/v1/favorites');
+var playlistsRouter = require('./routes/api/v1/playlists');
+var playlistFavoritesRouter = require('./routes/api/v1/playlistFavorites');
+
 app.use('/', indexRouter);
 app.use('/api/v1/favorites', favoritesRouter);
 app.use('/api/v1/playlists', playlistsRouter);
 app.use('/api/v1/playlists/:playlistId/favorites', playlistFavoritesRouter);
 
+
+//////////////
+//  API V2  //
+//////////////
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schemaBuilder');
 const root = require('./schema/resolvers')

241ec36627353d7faf527b48b65279105b3a4206
fentontaylor <fentontaylor@gmail.com>
2020-02-02 20:06:33 -0700

    Actually fix the mock musixService with mock nested promise

--- tests/utils/musixService.spec.js
+++ tests/utils/musixService.spec.js
@@ -4,9 +4,10 @@ const songJson = require('../../tests/fixtures/queenSong');
 const fetch = require('node-fetch')
 jest.mock('node-fetch');
 
+
 describe("MusixService", () => {
   it("can fetch song details from musixmatch API", async () => {
-    fetch.mockImplementation(() => Promise.resolve(songJson));
+    fetch.mockImplementation(() => Promise.resolve({ json: () => Promise.resolve(songJson) }));
 
     let res = await fetchSongInfo("We Will Rock You", "Queen");
     let track = res.message.body.track;
